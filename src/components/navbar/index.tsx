50eabf2364fbd41f490f1fae6c0df3804a638ae5
Abhishek <abhishekb740@gmail.com>
2024-07-09 15:23:13 +0530

    feat: Added Governance and Staking pages

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,14 +14,15 @@ export default function Navbar({ setShowWallet, showWallet }: Props) {
     const router = useRouter();
     const { wallet, connected } = useMetaMask();
     return (
-        <div className='flex flex-row h-[5rem] items-center justify-between px-16 border-b-[1px] border-b-[#B2F1A8] relative'  style={{fontFamily: 'Montserrat'}}>
+        <div className='flex flex-row h-[5rem] items-center justify-between px-16 border-b-[1px] border-b-[#B2F1A8] relative' style={{ fontFamily: 'Montserrat' }}>
             <div className='flex flex-row gap-2 hover:cursor-pointer' onClick={() => (router.push("/"))}>
                 <Image src='/TrustifyLogo.png' width={20} height={20} alt="Trustify Logo" />
                 <div className='text-2xl'>TRUSTYFI</div>
             </div>
             <div className='flex flex-row gap-4'>
+                <button className='border shadow-[0_0_5px_#B2F1A8] rounded-md py-1 px-3' onClick={() => router.push("/governance")}>Governance</button>
                 <button className='border shadow-[0_0_5px_#B2F1A8] rounded-md py-1 px-3' onClick={() => router.push("/categories")}>Categories</button>
-                <button className='border shadow-[0_0_5px_#B2F1A8] rounded-md py-1 px-3'>Stake</button>
+                <button className='border shadow-[0_0_5px_#B2F1A8] rounded-md py-1 px-3' onClick={() => router.push("/stake")}>Stake</button>
                 <button className='border shadow-[0_0_5px_#B2F1A8] rounded-md py-1 px-3'>BSC</button>
                 <button className='bg-[#B2F1A8] shadow-[0_0_5px_#B2F1A8] text-black rounded-md py-1 px-3 flex flex-row gap-2' onClick={connected ? () => (router.push(`/profile/${wallet.accounts}`)) : toggleWallet}>
                     <div>
